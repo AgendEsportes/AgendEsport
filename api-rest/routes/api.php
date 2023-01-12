9b30f84b777ba3182021cbc921570bb3f2f1b4b4
beaferreira <triszferreira2@gmail.com>
2023-01-12 11:05:07 -0300

    erro rotas api

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
 use App\Http\Controllers\DataEspacos;
-Route::controller('espacos', [DataEspacosController::class, 'index']);
-
 use App\Http\Controllers\DataEsportes;
-Route::controller('esportes', [DataEsportesController::class, 'store']);
+use App\Http\Controllers\AuthController;
+
+
+Route::apiResource([
+    'espacos' => DataEspacosController::class,
+]);
+Route::get('espacos', [DataEspacosController::class, 'espacos']);
+
+Route::apiResource('esportes', DataEsportesController::class);
+Route::get('esportes', [DataEsportesController::class, 'esportes']);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('logout', [AuthController::class, 'logout']);
+Route::get('user', [AuthController::class, 'user']);
+
